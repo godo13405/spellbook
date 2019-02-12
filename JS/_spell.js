0be740b1dc561f4583cb6c975866bcabd58ecc03
Gonçalo Andrade <goncaloandradedovale@gmail.com>
2019-02-12 18:37:32 +0000

    "What damage"

--- JS/_spell.js
+++ JS/_spell.js
@@ -4,23 +4,40 @@ const data = require('../data/spell.json'),
     tools = require('./_tools.js');
 
 const spell = {
-    description: ({intent,params}) => {
-        let output;
-
-        const subject = data[params.spell[0]];
-
-        output = `${tools.capitalize(params.spell[0])} is a level ${subject.level} which does `;
-
-        let damage = [];
-        subject.damage.forEach(x => {
-            damage.push(`${x.amount}${x.dice} ${x.type} damage`);
-        });
-        damage = damage.join(', ');
-
-        output += `${damage}.`;
-
+    description: ({
+        intent,
+        params,
+        subject = data[params.spell[0]]
+    }) => {
+        let output = `${tools.capitalize(subject.name)} is a level ${subject.level} spell`;
+        if (subject.damage) {
+            output +=` which does ${spell.tools.damage(subject)}.`;
+        }
 
         return output;
+    },
+    damage: ({
+        intent,
+        params,
+        subject = data[params.spell[0]]
+    }) => {
+        let output = `${tools.capitalize(params.spell[0])} does no damage.`;
+        if (subject.damage) {
+            output = `${tools.capitalize(params.spell[0])} does ${spell.tools.damage(subject)}.`;
+        }
+        
+        return output;
+    },
+    tools: {
+        damage: subject => {
+            let damage = [];
+            subject.damage.forEach(x => {
+                damage.push(`${x.amount}${x.dice} ${x.type} damage`);
+            });
+            damage = damage.join(', ');
+
+            return damage;
+        }
     }
 };
 
