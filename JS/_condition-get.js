f0fcfd7100b2174376fb2e9f010e052732ce609c
Gonçalo Andrade <goncaloandradedovale@gmail.com>
2019-02-19 23:17:03 +0000

    pcloud

--- JS/_condition-get.js
+++ JS/_condition-get.js
@@ -42,6 +42,44 @@ const get = {
 
         return output;
     },
+    mods: ({
+        intent,
+        params,
+        subject = data[params.condition]
+    }) => {
+        let vars = {
+            "name": `${subject.name}`,
+            "target": tools.targetRespond(params),
+        };
+
+        vars.cant = tools.getAll({
+            subject,
+            target: 'cant',
+            phrase: 'can\'t '
+        });
+
+        vars.autoFail = tools.getAll({
+            subject,
+            target: 'autoFail',
+            phrase: 'and automatically fail '
+        });
+
+        if (subject.roll) {
+            vars.mods = tools.capitalize(tools.getMods({
+                mods: subject.roll,
+                target: vars.target
+            }), true);
+        }
+
+        let output = {
+            data: tools.phrase({
+                phrase: intent.raw,
+                vars
+            })
+        };
+
+        return output;
+    },
 };
 
 
