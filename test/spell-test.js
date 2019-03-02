bb9256d1f4b580226b718e0697888b615a00e85e
Gonçalo Andrade <goncaloandradedovale@gmail.com>
2019-03-02 16:03:27 +0000

    more botium tests

--- test/spell-test.js
+++ test/spell-test.js
@@ -89,101 +89,101 @@ describe('Get spell', () => {
     //         assert.strictEqual(output.fulfillmentText, "Feign Death lasts for 1 hour");
     //     });
     // });
-    describe('components', () => {
-        // it('describe M, S, V', () => {
-        //     const req = new testTools.Req().
-        //     action("spell.get.components").
-        //     request;
-        //     const output = JSON.parse(router.ready(req));
-        //     assert.strictEqual(output.fulfillmentText, "Fireball needs some materials, an incantation, and gesturing");
-        // });
-        // it('describe S, V', () => {
-        //     const req = new testTools.Req().
-        //     action("spell.get.components").
-        //     name("spell", "dispel magic").
-        //     request;
-        //     const output = JSON.parse(router.ready(req));
-        //     assert.strictEqual(output.fulfillmentText, "Dispel Magic needs an incantation, and gesturing");
-        // });
-        // it('describe M, S', () => {
-        //     const req = new testTools.Req().
-        //     action("spell.get.components").
-        //     name("spell", "hypnotic pattern").
-        //     request;
-        //     const output = JSON.parse(router.ready(req));
-        //     assert.strictEqual(output.fulfillmentText, "Hypnotic Pattern needs some materials, and gesturing");
-        // });
-        it('describe V', () => {
-            const req = new testTools.Req().
-            action("spell.get.components").
-            name("spell", "mass healing word").
-            request;
-            req.queryResult.parameters.spell = ["mass healing word"];
-            const output = JSON.parse(router.ready(req));
-            assert.strictEqual(output.fulfillmentText, "Mass Healing Word needs an incantation");
-        });
-    });
-    describe('materials', () => {
-        it('yes', () => {
-            const req = new testTools.Req().
-            action("spell.get.materials").
-            request;
-            const output = JSON.parse(router.ready(req));
-            assert.strictEqual(output.fulfillmentText, "Fireball requires a tiny ball of bat guano and sulfur");
-        });
-        it('no', () => {
-            const req = new testTools.Req().
-            action("spell.get.materials").
-            name("spell", "dispel magic").
-            request;
-            const output = JSON.parse(router.ready(req));
-            assert.strictEqual(output.fulfillmentText, "Dispel Magic doesn't require any materials");
-        });
-    });
-    describe('classes', () => {
-        it('list', () => {
-            const req = new testTools.Req().
-            action("spell.get.classes").
-            request;
-            const output = JSON.parse(router.ready(req));
-            assert.strictEqual(output.fulfillmentText, "Fireball can be cast by Sorcerers, and Wizards");
-        });
-        it('single', () => {
-            const req = new testTools.Req().
-            action("spell.get.classes").
-            name('spell', 'elemental weapon').
-            request;
-            const output = JSON.parse(router.ready(req));
-            assert.strictEqual(output.fulfillmentText, "Elemental Weapon can only be cast by Paladins");
-        });
-    });
-    describe('school', () => {
-        it('list', () => {
-            const req = new testTools.Req().
-            action("spell.get.school").
-            request;
-            const output = JSON.parse(router.ready(req));
-            assert.strictEqual(output.fulfillmentText, "Fireball is an Evocation");
-        });
-    });
-    describe('description', () => {
-        it('describe', () => {
-            const req = new testTools.Req().
-            action("spell.get.description").
-            request;
-            const output = JSON.parse(router.ready(req));
-            assert.strictEqual(output.fulfillmentText, "When casting Fireball, a bright streak flashes from your pointing finger to a point you choose within range and then blossoms with a low roar into an explosion of flame. Each creature in a 20-foot-radius sphere centered on that point must make a Dexterity saving throw. A target takes **8d6 fire damage** on a failed save, or half as much damage on a successful one.<break time='200ms'/> The fire spreads around corners. It ignites flammable objects in the area that aren't being worn or carried.");
-        });
-    });
-    describe('level', () => {
-        it('describe', () => {
-            const req = new testTools.Req().
-            action("spell.get.level").
-            request;
-            const output = JSON.parse(router.ready(req));
-            assert.strictEqual(output.fulfillmentText, "Fireball is level 3");
-        });
-    });
+    // describe('components', () => {
+    //     // it('describe M, S, V', () => {
+    //     //     const req = new testTools.Req().
+    //     //     action("spell.get.components").
+    //     //     request;
+    //     //     const output = JSON.parse(router.ready(req));
+    //     //     assert.strictEqual(output.fulfillmentText, "Fireball needs some materials, an incantation, and gesturing");
+    //     // });
+    //     // it('describe S, V', () => {
+    //     //     const req = new testTools.Req().
+    //     //     action("spell.get.components").
+    //     //     name("spell", "dispel magic").
+    //     //     request;
+    //     //     const output = JSON.parse(router.ready(req));
+    //     //     assert.strictEqual(output.fulfillmentText, "Dispel Magic needs an incantation, and gesturing");
+    //     // });
+    //     // it('describe M, S', () => {
+    //     //     const req = new testTools.Req().
+    //     //     action("spell.get.components").
+    //     //     name("spell", "hypnotic pattern").
+    //     //     request;
+    //     //     const output = JSON.parse(router.ready(req));
+    //     //     assert.strictEqual(output.fulfillmentText, "Hypnotic Pattern needs some materials, and gesturing");
+    //     // });
+    //     // it('describe V', () => {
+    //     //     const req = new testTools.Req().
+    //     //     action("spell.get.components").
+    //     //     name("spell", "mass healing word").
+    //     //     request;
+    //     //     req.queryResult.parameters.spell = ["mass healing word"];
+    //     //     const output = JSON.parse(router.ready(req));
+    //     //     assert.strictEqual(output.fulfillmentText, "Mass Healing Word needs an incantation");
+    //     // });
+    // });s
+    // describe('materials', () => {
+    //     it('yes', () => {
+    //         const req = new testTools.Req().
+    //         action("spell.get.materials").
+    //         request;
+    //         const output = JSON.parse(router.ready(req));
+    //         assert.strictEqual(output.fulfillmentText, "Fireball requires a tiny ball of bat guano and sulfur");
+    //     });
+    //     it('no', () => {
+    //         const req = new testTools.Req().
+    //         action("spell.get.materials").
+    //         name("spell", "dispel magic").
+    //         request;
+    //         const output = JSON.parse(router.ready(req));
+    //         assert.strictEqual(output.fulfillmentText, "Dispel Magic doesn't require any materials");
+    //     });
+    // });
+    // describe('classes', () => {
+    //     it('list', () => {
+    //         const req = new testTools.Req().
+    //         action("spell.get.classes").
+    //         request;
+    //         const output = JSON.parse(router.ready(req));
+    //         assert.strictEqual(output.fulfillmentText, "Fireball can be cast by Sorcerers, and Wizards");
+    //     });
+    //     it('single', () => {
+    //         const req = new testTools.Req().
+    //         action("spell.get.classes").
+    //         name('spell', 'elemental weapon').
+    //         request;
+    //         const output = JSON.parse(router.ready(req));
+    //         assert.strictEqual(output.fulfillmentText, "Elemental Weapon can only be cast by Paladins");
+    //     });
+    // });
+    // describe('school', () => {
+    //     it('list', () => {
+    //         const req = new testTools.Req().
+    //         action("spell.get.school").
+    //         request;
+    //         const output = JSON.parse(router.ready(req));
+    //         assert.strictEqual(output.fulfillmentText, "Fireball is an Evocation");
+    //     });
+    // });
+    // describe('description', () => {
+    //     it('describe', () => {
+    //         const req = new testTools.Req().
+    //         action("spell.get.description").
+    //         request;
+    //         const output = JSON.parse(router.ready(req));
+    //         assert.strictEqual(output.fulfillmentText, "When casting Fireball, a bright streak flashes from your pointing finger to a point you choose within range and then blossoms with a low roar into an explosion of flame. Each creature in a 20-foot-radius sphere centered on that point must make a Dexterity saving throw. A target takes **8d6 fire damage** on a failed save, or half as much damage on a successful one.<break time='200ms'/> The fire spreads around corners. It ignites flammable objects in the area that aren't being worn or carried.");
+    //     });
+    // });
+    // describe('level', () => {
+    //     it('describe', () => {
+    //         const req = new testTools.Req().
+    //         action("spell.get.level").
+    //         request;
+    //         const output = JSON.parse(router.ready(req));
+    //         assert.strictEqual(output.fulfillmentText, "Fireball is level 3");
+    //     });
+    // });
     describe('healing', () => {
         it('no', () => {
             const req = new testTools.Req().
