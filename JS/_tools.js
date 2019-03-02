4337f7dccf980b9fe71d7c0091eb90deb497c424
Gonçalo Andrade <goncaloandradedovale@gmail.com>
2019-03-02 16:47:15 +0000

    botium tests

--- JS/_tools.js
+++ JS/_tools.js
@@ -274,6 +274,7 @@ const tools = {
                 action: parts[1],
                 function: parts[2],
             };
+        console.log(intent);
         intent.fn = require(`./_${intent.entity}.js`);
 
         return intent;
